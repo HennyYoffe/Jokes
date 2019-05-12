2282a38d9c1b61157e929a55b3cddaddbcf8abf3
Henny Yoffe <hennyy1149@gmail.com>
2019-05-12 13:22:06 -0400

    Add project files.

---
+++ HW62_Api_Jokes_May12/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HW62_Api_Jokes_May12.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
